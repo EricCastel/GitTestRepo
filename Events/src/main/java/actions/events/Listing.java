ede48a835caa49a043b2f20ee71f4c1c3f43b15e
EricCastel <l.socha@gmail.com>
2014-12-02 13:20:58 +0100

    Struts introduced

---
+++ Events/src/main/java/actions/events/Listing.java
@@ -0,0 +1,8 @@
+package actions.events;
+
+public class Listing {
+	public String execute(){
+		return "success";
+	}
+
+}
--- Events/src/main/webapp/index.jsp
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-<title>Index page</title></head>
-<body>
-
-<h2>testing...</h2>
-</body>
-</html>
